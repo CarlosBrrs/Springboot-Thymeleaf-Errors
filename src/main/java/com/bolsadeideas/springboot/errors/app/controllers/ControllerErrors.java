dc26cb897a8cc611f6684c9b115c5a09229220f3
Carlos Barrios Pereira <caebarri@bancolombia.com.co>
2022-03-14 01:27:40 -0500

    Arithmetic an NumberFormat exceptions handle

--- src/main/java/com/bolsadeideas/springboot/errors/app/controllers/ControllerErrors.java
+++ src/main/java/com/bolsadeideas/springboot/errors/app/controllers/ControllerErrors.java
@@ -9,6 +9,11 @@ public class ControllerErrors {
 
     @GetMapping("/index")
     public String index(Model model) {
+        //Linea para direccionar a template arithmetic
+        //Integer number = 100 / 0;
+
+        //Linea para direccionar a template numberFormat
+        Integer number = Integer.parseInt("10asd");
         return "index";
     }
 }
